d6edaffaec7e2e8e38ec7f4c9ceef29c39e7d3c8
Alex Bush <alexabush7@gmail.com>
2019-01-29 14:50:43 -0800

    completed main app functionality

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const state = {
   houseCount: {
-    h: 0,
-    s: 0,
-    r: 0,
-    h: 0
+    Griffindor: 0,
+    Slytherin: 0,
+    Ravenclaw: 0,
+    Hufflepuff: 0
   },
   questions: [],
   currentQuestionIndex: 0
@@ -13,6 +13,14 @@ window.addEventListener('load', function() {
   console.log('All assets are loaded');
   setUp();
 });
+
+/*
+  TODO
+  display answers properly,
+  when answer clicked, updated state and show next answer
+  add end condition
+  add display result
+*/
 
 // generate questions, build initial DOM state
 function setUp() {
@@ -24,15 +32,48 @@ function setUp() {
 function generateStartBtn(main) {
   const startBtn = document.createElement('button');
   startBtn.innerText = 'Start';
-  startBtn.addEventListener('click', () => displayQuestion(main));
+  startBtn.addEventListener('click', () => displayNextQuestion(main));
   main.appendChild(startBtn);
 }
 // gets next question from state, generates html based on the question, empties DOM, appends new question html to dom
-function displayQuestion(main) {
+function displayNextQuestion(main) {
   let question = getQuestion();
-  let questionHTML = generateQuestionHTML(question);
+  if (!question) displayResult(main);
+  else {
+    emptyNode(main);
+    main.appendChild(generateQuestionHTML(question));
+  }
+}
+
+function displayResult(main) {
   emptyNode(main);
-  main.appendChild(questionHTML);
+  let resultHTML = generateResultHTMl();
+  main.appendChild(resultHTML);
+}
+
+function generateResultHTMl() {
+  let resultContainer = document.createElement('div');
+  let resultsTitle = document.createElement('h2');
+  resultsTitle.innerText = 'Congratulations';
+  let results = document.createElement('h4');
+  results.innerText = `You are a ${getResult()}`;
+
+  resultContainer.appendChild(resultsTitle);
+  resultContainer.appendChild(results);
+  return resultContainer;
+}
+
+function getResult() {
+  let count = state.houseCount;
+  let highest = 0;
+  let houseWithHighestCount;
+  for (let house in count) {
+    if (count[house] > highest) {
+      highest++;
+      houseWithHighestCount = house;
+    }
+  }
+  return houseWithHighestCount;
 }
 
 function generateQuestionHTML(question) {
@@ -51,20 +92,53 @@ function generateQuestionHTML(question) {
   return questionContainer;
 }
 
-function generateAnswersHTML(answers) {
+// returns html: form -> radio inputs
+function generateAnswersHTML(answers = []) {
   // < input type = "radio" id = "answer house" name = "answer group" value = "answer text"
 
   let answersForm = document.createElement('form');
   answersForm.classList.add('answers');
+  answersForm.addEventListener('submit', handleAnswerSubmission);
   for (let answer of answers) {
-    let answerBtn = document.createElement('input');
-    answerBtn.setAttribute('type', 'radio');
-    answerBtn.setAttribute('value', answer.text);
-    answerBtn.setAttribute('id', answer.house);
-    answerBtn.setAttribute('name', 'answer');
-    answersForm.appendChild(answerBtn);
+    answersForm.appendChild(generateAnswerHTML(answer));
   }
+  let submitBtn = document.createElement('input');
+  submitBtn.setAttribute('type', 'submit');
+  submitBtn.setAttribute('value', 'Submit');
+  answersForm.appendChild(submitBtn);
   return answersForm;
+}
+
+function generateAnswerHTML(answer = {}) {
+  // <label for="huey">Huey</label>
+  let answerContainer = document.createElement('div');
+
+  let answerLabel = document.createElement('label');
+  answerLabel.setAttribute('for', answer.id);
+  answerLabel.innerText = answer.text;
+
+  let answerInput = document.createElement('input');
+  answerInput.setAttribute('type', 'radio');
+  answerInput.setAttribute('value', answer.text);
+  answerInput.setAttribute('id', answer.house);
+  answerInput.setAttribute('name', 'answer');
+
+  answerContainer.appendChild(answerLabel);
+  answerContainer.appendChild(answerInput);
+  return answerContainer;
+}
+
+// updates state to reflect new answer, displays next question/results
+function handleAnswerSubmission(e) {
+  let main = document.querySelector('main');
+  e.preventDefault();
+  let houseName = document.querySelector('input[name="answer"]:checked').id;
+  submitAnswer(houseName);
+  displayNextQuestion(main);
+}
+
+function submitAnswer(answer) {
+  state.houseCount[answer]++;
 }
 
 function getQuestion() {
@@ -81,40 +155,48 @@ function generateQuestions() {
   }
   let questions = [];
   questions.push(
-    new Question('q1', [
-      { text: 'a1', house: 'g' },
-      { text: 'a2', house: 's' },
-      { text: 'a3', house: 'r' },
-      { text: 'a4', house: 'h' }
+    new Question('Favorite Time of Day', [
+      { text: 'Morning', house: 'Hufflepuff' },
+      { text: 'Afternoon', house: 'Griffindor' },
+      { text: 'Dusk', house: 'Ravenclaw' },
+      { text: 'Night', house: 'Slytherin' }
     ]),
-    new Question('q2', [
-      { text: 'a1', house: 'g' },
-      { text: 'a2', house: 's' },
-      { text: 'a3', house: 'r' },
-      { text: 'a4', house: 'h' }
+    new Question('Favorite Smell', [
+      { text: 'Wood', house: 'Griffindor' },
+      { text: 'Ocean', house: 'Slytherin' },
+      { text: 'Paper', house: 'Ravenclaw' },
+      { text: 'Home', house: 'Hufflepuff' }
     ]),
-    new Question('q3', [
-      { text: 'a1', house: 'g' },
-      { text: 'a2', house: 's' },
-      { text: 'a3', house: 'r' },
-      { text: 'a4', house: 'h' }
+    new Question('Most Desirable', [
+      { text: 'To be trusted', house: 'Griffindor' },
+      { text: 'To be envied', house: 'Slytherin' },
+      { text: 'To be imitated', house: 'Ravenclaw' },
+      { text: 'To be praised', house: 'Hufflepuff' }
     ]),
-    new Question('q4', [
-      { text: 'a1', house: 'g' },
-      { text: 'a2', house: 's' },
-      { text: 'a3', house: 'r' },
-      { text: 'a4', house: 'h' }
+    new Question('Favorite Pet', [
+      { text: 'owl', house: 'Griffindor' },
+      { text: 'snake', house: 'Slytherin' },
+      { text: 'cat', house: 'Ravenclaw' },
+      { text: 'toad', house: 'Hufflepuff' }
+    ]),
+    new Question('Most desirable power?', [
+      { text: 'Flight', house: 'Griffindor' },
+      { text: 'Curses', house: 'Slytherin' },
+      { text: 'Apparation', house: 'Ravenclaw' },
+      { text: 'Transfiguration', house: 'Hufflepuff' }
     ])
   );
   return randomSort(questions);
 }
 
+// removes all elements from node
 function emptyNode(node) {
   while (node.firstChild) {
     node.removeChild(node.firstChild);
   }
 }
 
+// does algo to randomly sort array, returns array
 function randomSort(arr) {
   function shuffle(array) {
     var currentIndex = array.length,
